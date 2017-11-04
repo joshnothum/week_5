fa83f4494d4eb85a56ce5567ec8b89ea3e8bdaf1
Josh Nothum <joshnothum@JN.local>
2017-11-03 21:11:10 -0500

    working on sourcing controllers

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,2 +1,12 @@
 console.log('we ready');
 var myApp = angular.module('myApp', []);
+
+myApp.config(function ($routeProvider) {
+    $routeProvider.when('/rent', {
+        templateUrl: 'templates/rent.html',
+        controller: 'RentController as rc'
+    }).when('/sale', {
+        templateUrl: 'templates/sale.html',
+        controller: 'SaleController as sc'
+    });
+});
