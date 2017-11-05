335341f4522048907f489f2d25dd5599c620776c
Josh Nothum <joshnothum@gmail.com>
2017-11-05 12:13:44 -0600

    delete routes working for both sale and rental

--- server/public/scripts/controllers/forSale.controller.js
+++ server/public/scripts/controllers/forSale.controller.js
@@ -3,12 +3,13 @@ myApp.controller('SaleController', function ($http) {
 
     var sc = this;
     sc.newListing = {};
+    sc.allListings ={};
 
     sc.refreshListings = function () {
 
         $http.get('/list').then(function (success) {
             console.log(success);
-
+            sc.allListings = success.data;
         }).catch(function (error) {
             console.log(error);
 
@@ -19,7 +20,7 @@ myApp.controller('SaleController', function ($http) {
         console.log(addListing);
         $http.post('/list' , addListing).then(function (response) {
 
-
+            sc.refreshListings();
             console.log('addListing response:', response);
             
         }).catch(function (error) {
@@ -27,5 +28,17 @@ myApp.controller('SaleController', function ($http) {
             
         });
     };//end of addListing
-    sc.refreshListings();
+
+    sc.deleteListings = function (listId) {
+        console.log(listId);
+
+        $http.delete('/list/' + listId).then(function (response) {
+            console.log('deleteListings reponse:', response);
+            sc.refreshListings();
+
+        }).catch(function (error) {
+            console.log('deleteListings error:', error);
+
+        });
+    };//end of rc.addRentals()
 }); 
