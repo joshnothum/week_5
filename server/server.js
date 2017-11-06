2b32297cb7d088b1ce34cc27b6ca5c0f9e96914b
Josh Nothum <joshnothum@gmail.com>
2017-11-06 16:44:59 -0600

    push it to the cloud

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var rentals = require('./routers/rent.router.js');
 var listings = require('./routers/list.router.js');
 var port = process.env.PORT || 7000;
-var db = require('./modules/db.config.js');
+
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('server/public'));
@@ -16,7 +16,19 @@ app.use('/list', listings );
 
 // Eventually, the mongoose code should be in a module
 
+var mongoose = require('mongoose');
+// gamestop is the name of our database
+// 27017 is the default mongo port number
+var databaseUrl = 'mongodb://localhost:27017/realestate';
 
+mongoose.connection.on('connected', function () {
+    console.log('mongoose is connected!');
+});
+
+mongoose.connection.on('error', function () {
+    console.log('mongoose connection failed');
+});
+mongoose.connect(databaseUrl);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
