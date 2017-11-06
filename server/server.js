0fac65eb715c7f259285f152f319957c5499965f
Josh Nothum <joshnothum@gmail.com>
2017-11-06 16:33:41 -0600

    mongo moved to module

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var rentals = require('./routers/rent.router.js');
 var listings = require('./routers/list.router.js');
 var port = process.env.PORT || 7000;
-
+var db = require('./modules/db.config.js');
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('server/public'));
@@ -13,19 +13,7 @@ app.use(express.static('server/public'));
 app.use('/rent',rentals);
 app.use('/list', listings );
 /** ---------- MONGOOSE ------------ **/
-var mongoose = require('mongoose');
 
-// 27017 is the default mongo port number
-var databaseUrl = 'mongodb://localhost:27017/realestate';
-
-mongoose.connection.on('connected', function () {
-    console.log('mongoose is connected!', databaseUrl);
-});
-
-mongoose.connection.on('error', function (err) {
-    console.log('mongoose connection failed', err);
-});
-mongoose.connect(databaseUrl);
 // Eventually, the mongoose code should be in a module
 
 
